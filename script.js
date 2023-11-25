e7c47ed909300f27457025f21c02376ab93aca40
FerayGuluzada <fguluzada11276@ada.edu.az>
2023-11-25 20:37:28 +0400

    Data Fetch & Display

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
- 
+ fetch('https://dummyjson.com/products')
+.then(dummy => dummy.json())
+.then((dummy) => {
+
+    let card = "";
+    console.log(dummy);
+    dummy.products.map((product)=>{
+        card+=`<div class="card">
+        <h1 class="title">${product.title}</h1>
+        <img src="${product.images[0]}" alt="${product.thumbnail}">
+        <p>${product.description}</p>
+        <p class="category">${product.category}</p>
+        <p class="price">${product.price}</p>
+        <p class="discount">${product.discount}</p>
+        <p class="stock">${product.stock}</p>
+      </div>`
+    });
+
+    document.getElementById("cards").innerHTML = card;
+
+
+} ).catch((error)=>{console.log(error)});
+            
 
