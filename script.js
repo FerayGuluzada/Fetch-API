64cd47001d1bdc5bdb32af57e98bf841d1204b17
FerayGuluzada <fguluzada11276@ada.edu.az>
2023-11-27 01:02:03 +0400

    Minor Design Changes

--- script.js
+++ script.js
@@ -6,13 +6,13 @@
     console.log(dummy);
     dummy.products.map((product)=>{
         card+=`<div class="card">
-        <h1 class="title">${product.title}</h1>
         <img src="${product.images[0]}" alt="${product.thumbnail}">
+        <h1 class="title">${product.title}</h1>
         <p>${product.description}</p>
-        <p class="category">${product.category}</p>
-        <p class="price">${product.price}</p>
-        <p class="discount">${product.discount}</p>
-        <p class="stock">${product.stock}</p>
+        <p class="category"> <span class="categorySpan"> category: </span> ${product.category}</p>
+        <p class="price"> <span class="categorySpan"> price: </span> ${product.price}</p>
+        <p class="discount"><span class="categorySpan"> discount: </span>${product.discount}</p>
+        <p class="stock"> <span class="categorySpan"> stock: </span>${product.stock}</p>
       </div>`
     });
 
